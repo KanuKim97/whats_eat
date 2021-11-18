a4a4945712276156c4c5e1b731dee863f1798d2f
KanuKim97 <74421057+KanuKim97@users.noreply.github.com>
2021-11-19 01:14:01 +0900

    current_Location_tracking
    current_Location_tracking

---
+++ app/src/main/java/com/example/whats_eat/Maps.kt
@@ -0,0 +1,148 @@
+package com.example.whats_eat
+
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationRequest
+import android.os.Build
+import androidx.fragment.app.Fragment
+
+import android.os.Bundle
+import android.os.Looper
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import java.util.jar.Manifest
+
+class Maps : Fragment() {
+    private lateinit var mMap : GoogleMap
+
+    private var latitude : Double = 0.toDouble()
+    private var longitude : Double = 0.toDouble()
+
+    private lateinit var mLastLocation : Location
+    private var mMarker : Marker? = null
+
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    lateinit var locationRequest: com.google.android.gms.location.LocationRequest
+    lateinit var locationCallback: LocationCallback
+
+    companion object{
+        private const val MY_PERMISSION_CODE : Int = 1000
+    }
+
+
+    @SuppressLint("MissingPermission")
+    private val callback = OnMapReadyCallback { googleMap ->
+        mMap = googleMap
+        mMap.isMyLocationEnabled = true
+
+        mMap.uiSettings.isZoomControlsEnabled=true
+    }
+
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        checkPermission()
+
+
+        buildLocationRequest()
+        buildLocationCallBack()
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireContext())
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper())
+
+        return inflater.inflate(R.layout.fragment_maps, container, false)
+    }
+
+    private fun buildLocationCallBack() {
+        locationCallback = object : LocationCallback(){
+            override fun onLocationResult(p0: LocationResult) {
+                mLastLocation = p0!!.locations.get(p0!!.locations.size-1) //GetLastLocation
+
+                if (mMarker != null){
+                    mMarker!!.remove()
+                }
+
+                latitude = mLastLocation.latitude
+                longitude = mLastLocation.longitude
+
+                val latLng = LatLng(latitude, longitude)
+                val markerOptions = MarkerOptions()
+                        .position(latLng)
+                        .title("Your Here")
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
+
+                mMarker = mMap!!.addMarker(markerOptions)
+
+                mMap!!.moveCamera(CameraUpdateFactory.newLatLng(latLng))
+                mMap!!.moveCamera(CameraUpdateFactory.zoomBy(15f))
+            }
+        }
+    }
+
+    private fun buildLocationRequest() {
+        locationRequest = com.google.android.gms.location.LocationRequest()
+        locationRequest.priority = com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY
+        locationRequest.interval = 3000
+        locationRequest.fastestInterval = 1000
+        locationRequest.smallestDisplacement = 10f
+    }
+
+    private fun checkPermission() : Boolean {
+        if(ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED ) {
+                if(ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION))
+                    ActivityCompat.requestPermissions(requireActivity(), arrayOf(
+                                    android.Manifest.permission.ACCESS_FINE_LOCATION
+                    ), MY_PERMISSION_CODE)
+                else
+                    ActivityCompat.requestPermissions(requireActivity(), arrayOf(
+                            android.Manifest.permission.ACCESS_FINE_LOCATION
+                    ), MY_PERMISSION_CODE)
+                return false
+        } else
+            return true
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when(requestCode){
+            MY_PERMISSION_CODE -> {
+                if(grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    if(ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+                        if(checkPermission()) {
+                            mMap!!.isMyLocationEnabled = true
+                        }
+                }
+                else{
+                    Toast.makeText(requireContext(), "Permission Denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
+    }
+}
