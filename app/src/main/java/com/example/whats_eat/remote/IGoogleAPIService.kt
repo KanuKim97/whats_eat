a9da7c587e6cc5e4c400f55a8cc787ae7b7e93c0
KanuKim97 <74421057+KanuKim97@users.noreply.github.com>
2021-12-03 00:57:07 +0900

    mod viewPlace.kt
    mod viewPlace.kt

--- app/src/main/java/com/example/whats_eat/remote/IGoogleAPIService.kt
+++ app/src/main/java/com/example/whats_eat/remote/IGoogleAPIService.kt
@@ -1,6 +1,7 @@
 package com.example.whats_eat.remote
 
 import com.example.whats_eat.Model.Myplaces
+import com.example.whats_eat.Model.placeDetail
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Url
@@ -8,5 +9,8 @@ import retrofit2.http.Url
 
 interface IGoogleAPIService {
     @GET
-    fun getNearbyPlaces(@Url url:String) : Call<Myplaces>
+    fun getNearbyPlaces(@Url url : String) : Call<Myplaces>
+
+    @GET
+    fun getDetailPlace(@Url url : String) : Call<placeDetail>
 }
--- app/src/main/java/com/example/whats_eat/viewPlace.kt
+++ app/src/main/java/com/example/whats_eat/viewPlace.kt
@@ -1,14 +1,22 @@
 package com.example.whats_eat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.example.whats_eat.Common.Common
+import com.example.whats_eat.Common.Constant
 import com.example.whats_eat.Model.*
 import com.example.whats_eat.remote.IGoogleAPIService
 
 import com.example.whats_eat.databinding.ActivityViewPlaceBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.lang.StringBuilder
 
 class viewPlace : AppCompatActivity() {
     private lateinit var viewPlaceBinding : ActivityViewPlaceBinding
@@ -35,23 +43,61 @@ class viewPlace : AppCompatActivity() {
         }
 
         if(Common.currentPlace!!.rating != null) {
-            viewPlaceBinding.rating.numStars = Common.currentPlace!!.rating.toInt()
+            Log.d("rating", Common.currentPlace!!.rating.toString())
+            viewPlaceBinding.rating.rating = Common.currentPlace!!.rating.toFloat()
         } else {
             viewPlaceBinding.rating.visibility = View.GONE
         }
 
-        if(Common.currentPlace!!.rating != null) {
-            viewPlaceBinding.rating.numStars = Common.currentPlace!!.rating.toInt()
+        if(Common.currentPlace!!.opening_hours != null) {
+            val openTime : Boolean = Common.currentPlace!!.opening_hours!!.open_now
+
+            if(!openTime){
+                viewPlaceBinding.openTime.text = "영업 종료"
+            } else {
+                viewPlaceBinding.openTime.text = "영업 중"
+            }
+
         } else {
-            viewPlaceBinding.rating.visibility = View.GONE
+            viewPlaceBinding.openTime.visibility = View.GONE
         }
+
+        mService.getDetailPlace(getPlaceDetailUrl(Common.currentPlace!!.place_id))
+                .enqueue(object : Callback<placeDetail> {
+                    override fun onResponse(call: Call<placeDetail>, response: Response<placeDetail>) {
+                        mPlace = response.body()
+
+                        viewPlaceBinding.placeName.text = mPlace!!.result!!.name
+                        viewPlaceBinding.placeAddress.text = mPlace!!.result!!.formatted_address
+                    }
+
+                    override fun onFailure(call: Call<placeDetail>, t: Throwable) {
+                        Toast.makeText(baseContext, t.message, Toast.LENGTH_SHORT).show()
+                    }
+                })
 
         viewPlaceBinding.showMap.setOnClickListener {
-
+        // need to write it    
+        // startActivity(Intent(this, Maps::class.java))
         }
     }
 
-    private fun getPhotoPlace(photoReference: String, i: Int): String {
+    private fun getPlaceDetailUrl(placeId: String?): String {
+        val placeDetailUrl = StringBuilder("https://maps.googleapis.com/maps/api/place/details/json")
+        placeDetailUrl.append("?place_id=$placeId")
+        placeDetailUrl.append("&key=${Constant.API_KEYS}")
+
+        return placeDetailUrl.toString()
+    }
+
+    private fun getPhotoPlace(photoReference: String, maxWidth: Int): String {
+
+        val placePhotoUrl = StringBuilder("https://maps.googleapis.com/maps/api/place/photo")
+        placePhotoUrl.append("?maxwidth=$maxWidth")
+        placePhotoUrl.append("&photo_reference=$photoReference")
+        placePhotoUrl.append("&key=${Constant.API_KEYS}")
+
+        return placePhotoUrl.toString()
 
     }
 }
