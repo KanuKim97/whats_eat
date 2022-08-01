b96607406e687cc3cde967b15e3a27c6e9d0bb9f
KanuKim97 <74421057+KanuKim97@users.noreply.github.com>
2022-08-01 20:06:37 +0900

    Update Common.kt

--- app/src/main/java/com/example/whats_eat/data/common/Common.kt
+++ app/src/main/java/com/example/whats_eat/data/common/Common.kt
@@ -2,6 +2,4 @@ package com.example.whats_eat.data.common
 
 import com.example.whats_eat.data.model.commonModel.Results
 
-object Common {
-    var placeResultData: Results? = null
-}
+object Common { var placeResultData: Results? = null }
