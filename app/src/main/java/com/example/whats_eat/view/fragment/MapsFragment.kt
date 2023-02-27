0fa5338e799c33cfaab237c0f5981f6c633368bc
KanuKim97 <74421057+KanuKim97@users.noreply.github.com>
2023-02-27 22:00:25 +0900

    update initial MapsFragment.kt
    initial commit mapsFragment
    mod mapsFragmentView
     -> del <fragment> tag
     -> add <FrameLayout> tag

--- app/src/main/java/com/example/whats_eat/view/fragment/MapsFragment.kt
+++ app/src/main/java/com/example/whats_eat/view/fragment/MapsFragment.kt
@@ -1,32 +1,25 @@
 package com.example.whats_eat.view.fragment
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import android.Manifest
-import android.annotation.SuppressLint
-import android.location.Location
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
-import com.example.whats_eat.R
-import com.example.whats_eat.data.common.Constant
 import com.example.whats_eat.databinding.FragmentMapsBinding
 import com.example.whats_eat.viewModel.fragment.MapsViewModel
-import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import dagger.hilt.android.AndroidEntryPoint
-import pub.devrel.easypermissions.EasyPermissions
 
 @AndroidEntryPoint
-class MapsFragment: Fragment() {
-    private var _mapsFragmentBinding: FragmentMapsBinding?= null
+class MapsFragment: Fragment(), OnMapReadyCallback {
+    private lateinit var gMapView: MapView
+    private var _mapsFragmentBinding: FragmentMapsBinding? = null
     private val mapsFragmentBinding get() = _mapsFragmentBinding!!
     private val mapsViewModel: MapsViewModel by viewModels()
 
@@ -36,9 +29,42 @@ class MapsFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _mapsFragmentBinding = FragmentMapsBinding.inflate(inflater, container, false)
-
+        gMapView = mapsFragmentBinding.gMap
+        gMapView.onCreate(savedInstanceState)
+        gMapView.getMapAsync(this)
 
         return mapsFragmentBinding.root
+    }
+
+    override fun onMapReady(gMap: GoogleMap) {
+        val point = LatLng(37.514655, 126.979974)
+        gMap.addMarker(MarkerOptions().position(point).title("현위치"))
+        gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(point, 12f))
+    }
+
+    override fun onStart() {
+        super.onStart()
+        gMapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        gMapView.onResume()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        gMapView.onStop()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        gMapView.onLowMemory()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        gMapView.onDestroy()
     }
 
     override fun onDestroyView() {
